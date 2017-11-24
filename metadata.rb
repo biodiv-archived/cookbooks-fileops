a9713d5ca796748374f7ff0c014834d85768a4ec
Sravanthi <pearlsravanthi@gmail.com>
2017-11-24 11:33:55 +0530

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,43 +1,9 @@
-{
-  "name": "fileops",
-  "description": "Installs/Configures fileops on tomcat",
-  "long_description": "fileops Cookbook\n=============\nInstalls and configures fileops to run on tomcat application server.\n\nRequirements\n============\n\n## Platforms\n\n* Ubuntu 16.04 LTS\n\nTested on:\n\n* Ubuntu 16.04 LTS\n\n\n### Cookbooks\nRequires the following cookbooks\n\n* `cerner_tomcat`\n\nAttributes\n============\n\n\nRecipes\n=======\n* `default.rb` - Downloads solr and deploys it to tomcat.\n\nLicense and Author\n==================\n\n- Author:: Ashish Shinde (<ashish@strandls.com>)\n- Author:: Sandeep Tadekar (<sandeept@strandls.com>)\n- Author:: Prabhakar Rajagopal (<prabha@strandls.com>)\n\nLicensed under the Apache License, Version 2.0 (the \"License\");\nyou may not use this file except in compliance with the License.\nYou may obtain a copy of the License at\n\n    http://www.apache.org/licenses/LICENSE-2.0\n\nUnless required by applicable law or agreed to in writing, software\ndistributed under the License is distributed on an \"AS IS\" BASIS,\nWITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\nSee the License for the specific language governing permissions and\nlimitations under the License.\n",
-  "maintainer": "Strand Life Sciences",
-  "maintainer_email": "ashish@strandls.com",
-  "license": "Apache License, Version 2.0",
-  "platforms": {
-  },
-  "dependencies": {
-    "cerner_tomcat": "~> 3.1.0"
-  },
-  "recommendations": {
-  },
-  "suggestions": {
-  },
-  "conflicting": {
-  },
-  "providing": {
-  },
-  "replacing": {
-  },
-  "attributes": {
-  },
-  "groupings": {
-  },
-  "recipes": {
-  },
-  "version": "1.0.0",
-  "source_url": "",
-  "issues_url": "",
-  "privacy": false,
-  "chef_versions": [
+name             'fileops'
+maintainer       'Strand Life Sciences'
+maintainer_email 'ashish@strandls.com'
+license          'Apache License, Version 2.0'
+description      'Installs/Configures solr on tomcat'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '1.0.0'
 
-  ],
-  "ohai_versions": [
-
-  ],
-  "gems": [
-
-  ]
-}
-
+depends          'cerner_tomcat', '~> 3.1.0'
